9cdc8954fa9432e591b78b249d743a98126df890
VikrantKamble <62562434+vikrant-kamble@users.noreply.github.com>
2024-03-15 12:37:46 -0700

    update gitignore

---
+++ _site/assets/js/search-data.js
@@ -0,0 +1,41 @@
+var store = [{
+        "title": "PDF of a dependent variable",
+        "excerpt":"``The Calculus required continuity, and continuity was supposed to require the infinitely little; but nobody could discover what the infinitely little might be.\" &emsp;&emsp;&emsp;&emsp; -- Bertrand Russell in Mysticism and Logic and Other Essays, The Floating Press, 1 August 2010, p.100 Many-a-time when working with model fitting to data, one...","categories": ["statistics"],
+        "tags": [],
+        "url": "http://localhost:4000/statistics/pdf-of-function/"
+      },{
+        "title": "Poisson Process",
+        "excerpt":"Let’s imagine rain falling. One obvious parameter describing this process is the rate - whether its drizzling or pouring! Let’s now focus on a tiny patch of land and assume that the rate is constant and will term this as $\\lambda$. We can describe rain as a Poisson process. Now...","categories": ["statistics"],
+        "tags": [],
+        "url": "http://localhost:4000/statistics/poisson-process/"
+      },{
+        "title": "Logistic Regression",
+        "excerpt":"One of the most common test case in supervised machine learning is that of classification: Given a data point, classify it into one of the many labels available The simplest case of this is binary classification. For e.g. classifying emails as spam or ham,classifying objects as star or galaxy, classifying...","categories": ["statistics"],
+        "tags": [],
+        "url": "http://localhost:4000/statistics/logistic-regression/"
+      },{
+        "title": "Absorbing Markov Chains",
+        "excerpt":"A Markov chain containing absorbing states is known as an absorbing Markov chain. So what is an absorbing state. In simple words, if you end up on an absorbing state you can’t go anywhere else; you are stuck there for all eternity. In other words, the probability of transition from...","categories": ["statistics"],
+        "tags": [],
+        "url": "http://localhost:4000/statistics/absorbing-markov-chain/"
+      },{
+        "title": "Combinatorics, Tail sum for expectation and Simplices",
+        "excerpt":"1. How many different ways are possible to have $k-tuple$ with non-negative integer values such that they sum to a given value $n$? $\\to $To arrive at the solution, one can use the $stars-and-bars$ method. Let’s draw a sequence of n stars. There are $n-1$ spaces between the stars. What...","categories": ["statistics"],
+        "tags": [],
+        "url": "http://localhost:4000/statistics/combinatorics/"
+      },{
+        "title": "Arrays",
+        "excerpt":"One of the most basic things that one can do with an array is to have a pointer or a set of pointers thattraverse the array in some fashion. The number of pointers to book-keep depends on the question at hand.Let’s look at few sample problems to understand this concept:...","categories": ["algorithms"],
+        "tags": [],
+        "url": "http://localhost:4000/algorithms/sliding-windows/"
+      },{
+        "title": "IMDB movie reviews",
+        "excerpt":"The goal of this notebook is to understand some details about Huggingface’s dataset and transformer libraries and also as a reference point for fine-tuning a LLM model for a classification task. !pip install -q transformers datasets\u001b[2K \u001b[90m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\u001b[0m \u001b[32m510.5/510.5 kB\u001b[0m \u001b[31m7.7 MB/s\u001b[0m eta \u001b[36m0:00:00\u001b[0m\u001b[2K \u001b[90m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\u001b[0m \u001b[32m116.3/116.3 kB\u001b[0m \u001b[31m8.8 MB/s\u001b[0m eta \u001b[36m0:00:00\u001b[0m\u001b[2K...","categories": ["ML"],
+        "tags": [],
+        "url": "http://localhost:4000/ml/bert_movie_reviews/"
+      },{
+        "title": "ML Design",
+        "excerpt":"A ML design task can broadly be classified into the following components, in order of priority: Defining success metrics Data pipelines Backend model design Online model design Model evaluation Model monitoring ServingDefining success metrics The most important aspect when building a ML system for a specific task is to be...","categories": ["ML"],
+        "tags": [],
+        "url": "http://localhost:4000/ml/ml-design/"
+      }]
